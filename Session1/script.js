58f949a0600c914945ca47090d89e89e0163a38c
shvkii <92766830+shvkii@users.noreply.github.com>
2021-11-08 17:07:11 -0800

    Add files via upload

---
+++ Session1/script.js
@@ -0,0 +1,33 @@
+console.log("script")
+var user="Shakita";
+let age=99;//declaration and assign
+age=32
+let userName="Shakita"
+
+let student;//declaration
+student=true;//assign
+
+console.log("student");
+//annoying
+/*console.log("Name: " + userName + "\nAge: " + age + "\nStudent: " + student);*/
+//cool
+/*let tmp=`Name: ${userName} \n Age:${age}  \nStudent: ${student}`;*/
+
+
+
+//Exercise 1
+let numberChildren=1;
+let partnerName="Cory";
+let geoLocation="Michigan";
+let jobTitle="Influencer";
+
+document.write(`<h3>You will be an ${jobTitle} in ${geoLocation}, and married to ${partnerName} with ${numberChildren} kid.</h3>`)
+
+let inputName="Shakita";
+let emailAddress="shakita.thompson@gmail.com";
+let passWord=12345678;
+let ageYears=32;
+let country="United States";
+let annualSalary="75,000";
+
+document.write(`<h3>Name:  ${inputName}  \n Email: ${emailAddress} \nPassword: ${passWord}   \n Age: ${ageYears} \n Country: ${country} \n Salary: ${annualSalary}</h3>`)
