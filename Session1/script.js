71c1d0d0af31545f60d5772cbe1b24e3579cfae1
shvkii <92766830+shvkii@users.noreply.github.com>
2021-11-08 18:41:28 -0800

    Add files via upload

--- Session1/script.js
+++ Session1/script.js
@@ -29,5 +29,23 @@ let passWord=12345678;
 let ageYears=32;
 let country="United States";
 let annualSalary="75,000";
+let hairColor="Brown"
+let eyeColor="Brown"
+let birthPlace="Cherry Point, North Carolina"
+let numberofChildren="1"
+let nameofChild="Adrianna"
+let favoriteColor="teal"
+let favoriteDrink="Coffee"
+let favoriteFood="Lasagna"
+let favoritePastime="Gaming & Binging Netflix"
+let dreamVacation="Mykonos, Greece"
+let favoriteplaceVisited="Dubai,UAE"
+let futureGoals="Become a Multimillionaire"
+let booksReading="It's All In Your Head, by RUSS"
+let favoriteMovie="The Proposal"
+let dreamJob="Working from Laptop anywhere I want"
 
-document.write(`<h3>Name:  ${inputName}  \n Email: ${emailAddress} \nPassword: ${passWord}   \n Age: ${ageYears} \n Country: ${country} \n Salary: ${annualSalary}</h3>`)
+
+console.log(`Name:  ${inputName}\nEmail: ${emailAddress}\nPassword: ${passWord}\nAge: ${ageYears}\nCountry: ${country}\nSalary: ${annualSalary}\nHair Color: ${hairColor}\nEye Color: ${eyeColor}\nBirth Place: ${birthPlace}\nNumber of Children: ${numberofChildren}\nName of Child: ${nameofChild}\nFavorite Color: ${favoriteColor}\nFavorite Drink: ${favoriteDrink}\nFavorite Food: ${favoriteFood}\nFavorite Pastime: ${favoritePastime}\nDream Vacation: ${dreamVacation}\nFavorite Place Visited: ${favoritePastime}\nFuture Goals: ${futureGoals}\nCurrently Reading: ${booksReading}\nFavorite Movie: ${favoriteMovie}\nDream Job: ${dreamJob}`)
+
+document.write(`<h3> My name is ${inputName} my email address is  ${emailAddress}. I am ${ageYears} I live in the ${country}. I am a and I make ${annualSalary} a year. I have ${hairColor} hair and ${eyeColor} eyes. I was born on the Marine Corps base ${birthPlace}. I am a single mother and have  ${numberofChildren} child named ${nameofChild}. My favorite color is ${favoriteColor}. I just love the mixture of blue and green. I can't function without ${favoriteDrink}. My favorite food is ${favoriteFood} like Garfield. When I am not working or in class, I am usually  ${favoritePastime}. I am obsessed with Ryan Reynolds, so i love any movie he's in (even The Green Lantern) but my all time favorite is ${favoriteMovie} In the next year I hope to visit ${dreamVacation}. I really enjoyed visiting ${favoriteplaceVisited}. I cant wait to go back. Within the next 5 years I plan to ${futureGoals} & ${dreamJob}. The book I am currently reading is ${booksReading}. `)
